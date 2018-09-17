af81598ffc9c5546c023b934051c8b6983e65d29
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-09-17 05:49:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,29 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
 });
+
+function getIt() {
+  $('p').on('click', function () {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('You pressed the G key!');
+    }
+  })
+}
+
+function submitIt() {
+  $()
+}
